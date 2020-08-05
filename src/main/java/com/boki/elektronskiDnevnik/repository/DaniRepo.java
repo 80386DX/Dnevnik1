737e81e104dc8d37961bae21e9285ed0abf0e786
80386DX <>
2020-08-05 05:01:21 +0200

    Pocetak Repository

--- src/main/java/com/boki/elektronskiDnevnik/repository/DaniRepo.java
+++ src/main/java/com/boki/elektronskiDnevnik/repository/DaniRepo.java
@@ -1,4 +1,8 @@
 package com.boki.elektronskiDnevnik.repository;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 public interface DaniRepo extends JpaRepository <Dani, Integer> {
     
