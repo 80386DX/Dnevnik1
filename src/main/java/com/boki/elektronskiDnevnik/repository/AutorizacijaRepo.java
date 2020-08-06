4c531e1719793ecbe3e68cb66b7edded947bf165
80386DX <>
2020-08-06 03:07:42 +0200

    Full repository

--- src/main/java/com/boki/elektronskiDnevnik/repository/AutorizacijaRepo.java
+++ src/main/java/com/boki/elektronskiDnevnik/repository/AutorizacijaRepo.java
@@ -1,7 +1,9 @@
 package com.boki.elektronskiDnevnik.repository;
 import com.boki.elektronskiDnevnik.entity.Autorizacija;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AutorizacijaRepo extends JpaRepository <Autorizacija, Integer> {
     
+    List <Autorizacija> findByRang(String rang);
 }
