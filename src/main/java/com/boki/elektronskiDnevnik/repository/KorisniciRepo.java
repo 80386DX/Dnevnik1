8f7d92e0d6c88a1d69f2048af04b3fa4b9249d5f
80386DX <>
2020-08-08 05:17:55 +0200

    Service

--- src/main/java/com/boki/elektronskiDnevnik/repository/KorisniciRepo.java
+++ src/main/java/com/boki/elektronskiDnevnik/repository/KorisniciRepo.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface KorisniciRepo extends JpaRepository <Korisnici, Integer> {
     
+    
 }
---
+++ src/main/java/com/boki/elektronskiDnevnik/service/KorisniciService.java
@@ -0,0 +1,32 @@
+package com.boki.elektronskiDnevnik.service;
+
+import com.boki.elektronskiDnevnik.entity.Korisnici;
+import com.boki.elektronskiDnevnik.repository.KorisniciRepo;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KorisniciService {
+    
+    @Autowired
+    KorisniciRepo repo;
+    
+     
+    public List<Korisnici> listAll() {
+        return repo.findAll();
+    }
+    
+    public void save(Korisnici korisnik){
+        repo.save(korisnik);
+    }
+    
+    public void getById (Integer id){
+        repo.findById(id);
+    }
+    
+    public void delete(Integer id){
+        repo.deleteById(id);
+    }
+    
+}
