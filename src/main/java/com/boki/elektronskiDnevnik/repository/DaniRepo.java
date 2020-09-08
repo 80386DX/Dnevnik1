0469baf0e7dddc6f7c60df02475421da4b038ece
80386DX <>
2020-09-08 16:23:01 +0200

    ...

--- src/main/java/com/boki/elektronskiDnevnik/repository/DaniRepo.java
+++ src/main/java/com/boki/elektronskiDnevnik/repository/DaniRepo.java
@@ -1,10 +1,12 @@
 package com.boki.elektronskiDnevnik.repository;
 import com.boki.elektronskiDnevnik.entity.Dani;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 public interface DaniRepo extends JpaRepository <Dani, Integer> {
     
+    Optional <Dani> findByName(String ime);
 }
